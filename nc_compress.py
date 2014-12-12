fdd8fd74baf9660c2f0a7ee6f9f132b46feb8822
Aidan Heerdegen <aidan.heerdegen@gmail.com>
2014-12-12 15:43:03 +1100

    Added in chunking calculation routines to keep self-contained

--- nc_compress.py
+++ nc_compress.py
@@ -8,6 +8,99 @@ import re
 from warnings import warn
 from shutil import move
 from collections import defaultdict
+import math
+import operator
+import numpy as np
+import numpy.ma as ma
+
+def numVals(shape):
+    """Return number of values in chunk of specified shape, given by a list of dimension lengths.
+
+    shape -- list of variable dimension sizes"""
+    if(len(shape) == 0):
+        return 1
+    return reduce(operator.mul, shape)
+
+def calcChunkShape(chunkVol, varShape):
+    """
+    Calculate a chunk shape for a given volume/area for the dimensions in varShape.
+
+    chunkVol   -- volume/area of the chunk
+    chunkVol   -- array of dimensions for the whole dataset
+    """
+
+    return np.array(np.ceil(np.asarray(varShape) * (chunkVol / float(numVals(varShape))) ** (1./len(varShape))),dtype="int")
+
+def chunk_shape_nD(varShape, valSize=4, chunkSize=4096, minDim=2):
+    """
+    Return a 'good shape' for an nD variable, assuming balanced 1D, 2D access
+
+    varShape  -- list of variable dimension sizes
+    chunkSize -- minimum chunksize desired, in bytes (default 4096)
+    valSize   -- size of each data value, in bytes (default 4)
+    minDim    -- mimimum chunk dimension (if var dimension larger
+                 than this value, otherwise it is just var dimension)
+
+    Returns integer chunk lengths of a chunk shape that provides
+    balanced access of 1D subsets and 2D subsets of a netCDF or HDF5
+    variable var. 'Good shape' for chunks means that the number of
+    chunks accessed to read any kind of 1D or 2D subset is approximately
+    equal, and the size of each chunk (uncompressed) is at least
+    chunkSize, which is often a disk block size.
+    """
+
+    import pdb
+
+    varShapema = ma.array(varShape)
+    
+    chunkVals = chunkSize / float(valSize) # ideal number of values in a chunk
+
+    # Make an ideal chunk shape array 
+    chunkShape = ma.array(calcChunkShape(chunkVals,varShapema),dtype=int)
+
+    # And a copy where we'll store our final values
+    chunkShapeFinal = ma.masked_all(chunkShape.shape,dtype=int)
+
+    lastChunkCount = -1
+    
+    while True:
+
+        # Loop over the axes in chunkShape, making sure they are at
+        # least minDim in length.
+        for i in range(len(chunkShape)):
+            if ma.is_masked(chunkShape[i]):
+                continue 
+            if (chunkShape[i] < minDim):
+                # Set the final chunk shape for this dimension
+                chunkShapeFinal[i] = min(minDim,varShapema[i])
+                # mask it out of the array of possible chunkShapes
+                chunkShape[i] = ma.masked
+
+        # print chunkShape,chunkShapeFinal
+        # Have we fixed any dimensions and filled them in chunkShapeFinal?
+        if chunkShapeFinal.count() > 0:
+            chunkCount = numVals(chunkShapeFinal[~chunkShapeFinal.mask])
+        else:
+            if (lastChunkCount == -1):
+                # Haven't modified initial guess, break out of
+                # this loop and accept chunkShape 
+                break
+
+        if chunkCount != lastChunkCount:
+            # Recalculate chunkShape array, with reduced dimensions
+            chunkShape[~chunkShape.mask] = calcChunkShape(chunkVals/chunkCount,varShapema[~chunkShape.mask])
+            lastChunkCount = chunkCount
+        else:
+            break
+
+
+    # This doesn't work when chunkShape has no masked values. Weird.
+    # chunkShapeFinal[chunkShapeFinal.mask] = chunkShape[~chunkShape.mask]
+    for i in range(len(chunkShapeFinal)):
+        if ma.is_masked(chunkShapeFinal[i]):
+            chunkShapeFinal[i] = chunkShape[i]
+
+    return chunkShapeFinal
 
 # A couple of hard-wired executable paths that might need changing
 
@@ -21,6 +114,17 @@ def maxcompression_type(x):
         raise argparse.ArgumentTypeError("Minimum maxcompression is 0")
     return x
 
+def get_dimensions(ncfile):
+    tmp = nc.Dataset(ncfile)
+    for dim in tmp.dimensions:
+        print dim
+
+def is_compressed(ncfile):
+    tmp = nc.Dataset(ncfile)
+    compressed=False
+    for var in tmp.variables:
+        print var,getattr(tmp.variables[var],'_DeflateLevel')
+        
 def run_nccopy(infile,outfile,level,limited,shuffle,chunking=None):
     # The format string for the time command
     #   %e     (Not in tcsh.) Elapsed real time (in seconds).
